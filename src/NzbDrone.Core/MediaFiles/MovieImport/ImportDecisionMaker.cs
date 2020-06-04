4fc48b89dc345e535feb2143baa4863a08c535ec
pablocp19 <62502825+pablocp19@users.noreply.github.com>
2020-06-04 17:38:15 +0200

    Restore the file

--- src/NzbDrone.Core/MediaFiles/MovieImport/ImportDecisionMaker.cs
+++ src/NzbDrone.Core/MediaFiles/MovieImport/ImportDecisionMaker.cs
@@ -163,22 +163,8 @@ namespace NzbDrone.Core.MediaFiles.MovieImport
                         }
 
                     }
-                    //decision = new ImportDecision(localMovie, new Rejection("Unable to parse file"));
-                    //TODO: make it so media info doesn't ruin the import process of a new movie
-                    var mediaInfo = (_config.EnableMediaInfo || !movie.Path?.IsParentPath(file) == true) ? _videoFileInfoReader.GetMediaInfo(file) : null;
-                    var size = _diskProvider.GetFileSize(file);
-                    var historyItems = _historyService.FindByDownloadId(downloadClientItem?.DownloadId ?? "");
-                    var firstHistoryItem = historyItems?.OrderByDescending(h => h.Date)?.FirstOrDefault();
-                    var sizeMovie = new LocalMovie();
-                    sizeMovie.Size = size;
-                    localMovie = _parsingService.GetLocalMovie(file, minimalInfo, movie, new List<object>{mediaInfo, firstHistoryItem, sizeMovie, folderInfo}, sceneSource);
-                    localMovie.Quality = GetQuality(folderInfo, localMovie.Quality, movie);
-                    localMovie.Size = size;
 
-                    _logger.Debug("Size: {0}", localMovie.Size);
-
-                    decision = GetDecision(localMovie, downloadClientItem);
-                    //decision = new ImportDecision(localMovie, new Rejection("Unable to parse file"));
+                    decision = new ImportDecision(localMovie, new Rejection("Unable to parse file"));
                 }
             }
             catch (Exception e)
@@ -186,7 +172,7 @@ namespace NzbDrone.Core.MediaFiles.MovieImport
                 _logger.Error(e, "Couldn't import file. {0}", file);
 
                 var localMovie = new LocalMovie { Path = file };
-                //decision = new ImportDecision(localMovie, new Rejection("Unexpected error processing file"));
+                decision = new ImportDecision(localMovie, new Rejection("Unexpected error processing file"));
             }
 
             //LocalMovie nullMovie = null;
